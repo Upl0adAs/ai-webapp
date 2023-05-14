aef407176ca862ddb78ff27a02822546a85ae6a5
Upl0adAs <128997986+Upl0adAs@users.noreply.github.com>
2023-05-14 15:51:31 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+song = "music.mp3";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+
+song = "music2.mp3";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+
+function preload()
+{
+  song = loadSound("music.mp3");
+  song = loadSound("music2.mp3");
+}
+
+function setup() {
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function draw() {
+    image(video, 0, 0, 600, 500);
+}
