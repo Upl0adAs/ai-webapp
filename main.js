86f2e9b8ccb3fb805d90a47ea9f82f6a672276e0
Upl0adAs <128997986+Upl0adAs@users.noreply.github.com>
2023-05-27 12:42:35 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -26,33 +26,6 @@ function setup() {
     poseNet = ml5.poseNet(video, modelLoaded);
     poseNet.on('pose', gotPoses);
 }
-
-function modelLoaded() {
-    console.log('PoseNet Is Initialized');
-}
-
-function gotPoses(results)
-{
-    if(results.length > 0)
-    {
-        console.log(results);
-        leftWristX = results[0].pose.leftWrist.x;
-        leftWristY = results[0].pose.leftWrist.y;
-        console.log("leftWrist = " + leftWristX +"leftWristY = "+ leftWristY)
-
-        rightWristX = results[0].pose.rightWrist.x;
-        rightWristY = results[0].pose.rightWrist.y;
-        console.log("rightWrist = " + rightWristX +"rightWristY = "+ rightWristY)
-    }
-}
-
 function draw() {
     image(video, 0, 0, 600, 500);
-}
-
-function play()
-{
-    song.play()
-    song.setVolume(1);
-    song.rate(1);
 }
